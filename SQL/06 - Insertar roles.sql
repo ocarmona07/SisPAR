9ceec3f13bee5f1d487f2fbca2a25df3abc88aa0
Omar Carmona Rivas <ocarmona07@gmail.com>
2014-05-05 21:48:20 -0400

    Se actualiza roles

--- SQL/06 - Insertar roles.sql
+++ SQL/06 - Insertar roles.sql
@@ -1,13 +1,9 @@
  -- Insertar roles
-USE SisPAR
- 
+ USE SisPAR; 
  
  INSERT INTO dbo.ROL_ROL
-                 ( ROL_NOMBRE, ROL_DESCRIPCION )
-         VALUES  ( 'Administrador', -- ROL_NOMBRE - varchar(25)
-                   'Usuario principal que maneja el sistema'  -- ROL_DESCRIPCION - varchar(max)
-                   ),
-                   (
-                   'Usuario',
-                   'usuario del Sistema'
-                   )
+        ( ROL_NOMBRE, ROL_DESCRIPCION )
+ VALUES ( 'Administrador', -- ROL_NOMBRE - varchar(25)
+          'Usuario principal que maneja el sistema'  -- ROL_DESCRIPCION - varchar(max)
+          ),
+        ( 'Usuario', 'usuario del Sistema' )
